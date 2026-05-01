6eda93665049506af64e15c25737bf019e90e2b9
Muhammad Asad Ullah <asadsagheer243@gmail.com>
2026-05-01 14:59:26 +0500

    feat: Added the correct api parameter

--- core/llm.py
+++ core/llm.py
@@ -4,5 +4,8 @@ from config.settings import GEMINI_API_KEY, model
 # ------------------------------
 # Gemini LLM
 # ------------------------------
-
-llm = GoogleGenerativeAI(model=model, temperature=0.3, api_key=GEMINI_API_KEY)
+if not GEMINI_API_KEY:
+    ll=None
+    raise ValueError("GEMINI_API_KEY is not set. Please set it in Streamlit Secrets or as an environment variable.")
+else:
+    llm = GoogleGenerativeAI(model=model, temperature=0.3, google_api_key=GEMINI_API_KEY)
